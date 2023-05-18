80ba1ed7004172011e3753a8c83be82854690ee6
Marlin Reams <marlinreams@gmail.com>
2023-05-18 15:53:19 -0500

    Time: 29 ms (90.92%) | Memory: 13.3 MB (97.05%) - LeetSync

--- roman-to-integer/roman-to-integer.py
+++ roman-to-integer/roman-to-integer.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def romanToInt(self, s):
-        romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000,
-        "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
+        romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         total = 0
         prev_value = 0
         for i in reversed(s):
